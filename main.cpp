62793d74f92d7559840abf8a204eb7dc16b9086b
raresciocirlan <93649524+raresciocirlan@users.noreply.github.com>
2022-08-16 19:39:58 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>      /* for printf() and fprintf() */
 #include <stdlib.h>      
 
-/*#include <dlt/dlt.h>
+#include <dlt/dlt.h>
 
 #define DLT_INT32(INT_VAR)(void)dlt_user_log_write_int32(&log_local,INT_VAR)
 
@@ -11,30 +11,33 @@
 #define DLT_BOOL(BOOL_VAR)(void)dlt_user_log_write_bool(&log_local,BOOL_VAR)
 
 
- 
 
-DLT_DECLARE_CONTEXT(con_example1);*/
+
+DLT_DECLARE_CONTEXT(con_example1);
 
 
 int main()
 {
-    /*DLT_REGISTER_APP("Exemplu", "Primul exemplu");
-    DLT_REGISTER_CONTEXT(con_example1, "CON", "First context");*/
+    DLT_REGISTER_APP("Exemplu", "Primul exemplu");
+    DLT_REGISTER_CONTEXT(con_example1, "CON", "First context");
 
+    int file_size;
     char buff[10];
-    FILE *fin = fopen("file.txt","rb");
-    size_t readlen = fread( buff, 1, sizeof(buff), fin );
+    ifstream file ("file.txt", std::ifstream::binary);
+    file.seekg(0, ifstream::end);
+    file_size = file.tellg();
+    file.seekg(0);
+    while(file.eof() != true)
+    {
+        file.read(buff,10);
+        DLT_LOG(con_example1, DLT_LOG_INFO, DLT_STRING("file.txt"), DLT_INT(file_size), DLT_INT32(10), DLT_ARG_RAW(buff));
 
+    }
 
-/*int i,n;
- 
-for (i=0;i<=9;i++)
-{
-DLT_LOG(con_example1, DLT_LOG_INFO, DLT_STRING("Message nr "),DLT_INT32(i));
-}
-*/
+    file.close();
 
+    return 0;
 
-   /*DLT_UNREGISTER_CONTEXT(con_example1);
-    DLT_UNREGISTER_APP();*/
+    DLT_UNREGISTER_CONTEXT(con_example1);
+    DLT_UNREGISTER_APP();
 }
