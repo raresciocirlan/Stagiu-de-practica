1e8fc8fed2761ab03dfe9f3f9004585d8fc6800a
raresciocirlan <93649524+raresciocirlan@users.noreply.github.com>
2022-08-18 16:34:36 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
-//#include <stdio.h>      /* for printf() and fprintf() */
-//#include <stdlib.h>
 #include <fstream>
-
-
 #include <dlt/dlt.h>
 
 #define DLT_INT32(INT_VAR)(void)dlt_user_log_write_int32(&log_local,INT_VAR)
-
 #define DLT_FLOAT32(FLOAT32_VAR)(void)dlt_user_log_write_float32(&log_local,FLOAT32_VAR)
-
 #define DLT_BOOL(BOOL_VAR)(void)dlt_user_log_write_bool(&log_local,BOOL_VAR)
-
 #define DLT_RAW(BUF, LEN)(void)dlt_user_log_write_raw(&log_local, BUF, LEN);
-
 
 using namespace std;
 
-
 DLT_DECLARE_CONTEXT(flst_example2);
-
 
 int main()
 {
@@ -27,19 +17,30 @@ int main()
     DLT_REGISTER_CONTEXT(flst_example2, "FLST", "Second context");
 
     int file_size, id;
+
+    //Generarea unui numar la intamplare
     srand(time(NULL));
     id = rand();
-    char buff[10];
+
+
+    char buff[10]; //Buffer de memorie
+
+
     ifstream file ("file.txt", std::ifstream::binary);
+
     file.seekg(0, ifstream::end);
     file_size = file.tellg();
     file.seekg(0);
+    //Pentru marimea fisierului
+
     DLT_LOG(flst_example2, DLT_LOG_INFO, DLT_STRING("FLST"), DLT_INT(id),DLT_STRING("file.txt"), DLT_INT(file_size));
+
     while(file.eof() != true)
     {
-        file.read(buff,10);
-        DLT_LOG(flst_example2, DLT_LOG_INFO, DLT_STRING("FLDA"), DLT_INT(id), DLT_RAW(buff, 10));
+        file.read(buff,10);//Se citeste fisierul cu ajutorul unui buffer de 10 bytes
 
+        //Buffer-ul se trimite la dlt
+        DLT_LOG(flst_example2, DLT_LOG_INFO, DLT_STRING("FLDA"), DLT_INT(id), DLT_RAW(buff, 10));
     }
 
     file.close();
